b1f72f36ee01977ab3c6ddc2382176461949cef1
Mohammad Namakshenas <m.n.shenas@gmail.com>
2024-04-10 14:52:25 +0200

    Add minor changes

--- classic_method.py
+++ classic_method.py
@@ -1,20 +1,20 @@
 import numpy as np
 from gurobipy import Model, GRB
 
-# CONSTANTs
-N_LOC = 3
-N_MARKET = 5
-
-# SETs
-s_loc = range(N_LOC)
-s_market = range(N_MARKET)
-
 # PARAMETERs (only here should be changed)
 p_fix_cost = [120, 150, 135]
 p_demand = [180, 170, 250, 200, 190]
 p_transport_cost = [[10, 20, 15, 16, 17],
                     [5, 17, 18, 15, 15],
                     [13, 15, 18, 15, 16]]
+
+# CONSTANTs
+N_LOC = len(p_fix_cost)
+N_MARKET = len(p_demand)
+
+# SETs
+s_loc = range(N_LOC)
+s_market = range(N_MARKET)
 
 # INSTANTIATE THE MODEL
 model = Model()
@@ -24,8 +24,6 @@ v_locate = model.addVars(N_LOC, vtype=GRB.BINARY)
 v_transport = model.addVars(N_LOC, N_MARKET)
 
 # ASSERT DIMENSIONAL CONSISTENCY
-assert len(p_fix_cost) == N_LOC
-assert len(p_demand) == N_MARKET
 assert len(p_transport_cost) == N_LOC
 assert len(p_transport_cost[0]) == N_MARKET
 
